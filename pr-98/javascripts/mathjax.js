99d2db7119d9a2d89bb34095ee287d8bbe3cb929
nampham2 <nampham2@users.noreply.github.com>
2026-03-10 20:02:02 +0000

    deploy: 996a5f0d5ff5f4c4e3d74bfc7e201e6190eaeaef

---
+++ pr-98/javascripts/mathjax.js
@@ -0,0 +1,14 @@
+/* MathJax Configuration for DQX Documentation */
+
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
